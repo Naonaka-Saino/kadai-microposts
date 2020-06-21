623801f4a861fb4e13b701ea647b4b3655363416
nao.saino <saino305@gmail.com>
2020-06-21 08:33:00 +0000

    dependent

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,8 +2,8 @@ class Micropost < ApplicationRecord
   belongs_to :user
   validates :content, presence: true, length: { maximum: 255 }
   #いいね機能
-  has_many :favorites
-  has_many :favorite_users, through: :favorites, source: :user#?
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user, dependent: :destroy#?
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
